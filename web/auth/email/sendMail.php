033e9a2c1c6f0faaee1e9b3cf17c7cf38b9b71e5
Valentin Lemaitre <valentinlemaitre06@gmail.com>
2025-11-13 09:26:45 +0100

    ajout des headers

--- web/auth/email/sendMail.php
+++ web/auth/email/sendMail.php
@@ -10,8 +10,14 @@
 				$destinataire = $email;
 				$message = "Bienvenue $userName sur To Do list. Votre code de validation est $code";
 
+				// --- Configuration des headers ---
+				$from = "do-not-reply@airbot.adkynet.eu";
+				$headers = "From: ToDoList <$from>\r\n";
+				$headers .= "Reply-To: $from\r\n";
+				$headers .= "X-Mailer: PHP/" . phpversion();
+				$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-				mail($destinataire, $subject,$message);
+				mail($destinataire, $subject,$message, $headers);
 
 				return true;
 
